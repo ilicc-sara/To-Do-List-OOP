5a3ac6e32f0e341624e57e83dff4b4619c8895f5
Sara <sarailic431@gmail.com>
2025-02-18 15:55:16 +0100

    ...

--- src/main.js
+++ src/main.js
@@ -41,16 +41,17 @@ const allToDos = new ManagerCreator();
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  inputNameEl.value = "";
   const newToDo = new ToDo(inputName);
-
   allToDos.set(newToDo);
 
-  console.log(allToDos.toDos);
-
   const item = document.createElement("li");
-
   item.innerHTML = `${inputName} <div class="btn-cont"><button class="btn-done">DONE</button><button class="btn-del">DELETE</button></div>`;
   item.setAttribute("data-id", newToDo.id);
   toDoListContainer.appendChild(item).className = "to-do";
+  inputNameEl.value = "";
+});
+
+toDoListContainer.addEventListener("click", function (e) {
+  // prettier-ignore
+  if (!e.target.classList.contains("btn-done") && !e.target.classList.contains("btn-del")) return;
 });
